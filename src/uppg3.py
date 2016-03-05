f8618d145d98cd38d261d6d3a3bbd810fc94e82b
essenji <essenjisimon@gmail.com>
2016-03-05 17:00:31 +0100

    Completed the third assignment, added nice comments as well

--- src/uppg3.py
+++ src/uppg3.py
@@ -4,45 +4,53 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    L = n.array([1, 0.1, 0.01, 0.001])
+    """
+    This program calculates the Fraunhofer and Fresnel graphs given different distances L. Fresnel uses N as number of slits.
+    :return:
+    """
+    L = n.array([1, 0.1, 0.01, 0.001]) # Distance from screen to slit
+    N = 20 # Number of slits (only used in Fresnel)
     x = [n.linspace(-0.02*i, 0.02*i, 1000) for i in L]
-    N = 2
     for index, i in enumerate(L):
         plt.subplot(2,2,1+index)
-        plt.plot(x[index], fraunhofer(x[index], i))
-        plt.plot(x[index], fresnel(x[index], i, N))
-
+        plt.plot(x[index], fraunhofer(x[index], i), label="Fraunhofer")
+        plt.plot(x[index], fresnel(x[index], i, N), label="Fresnel")
+        plt.xlabel("x")
+        plt.ylabel("Intensity")
+        plt.axis([-0.02*i,0.02*i, 0, 1])
+        plt.legend()
     plt.show()
-    # plt.subplot(2,2,1)
-    # plt.plot(x, fraunhofer(x, 1), label="Distance 1m")
-    # plt.plot(x, fresnel(x, 1, N), label="Distance 1m")
-    # x = n.linspace(-0.001, 0.001, 1000)
-    # plt.subplot(2,2,2)
-    # plt.plot(x, fraunhofer(x, 0.1))
-    # plt.plot(x, fresnel(x, 0.1, N))
-    # x = n.linspace(-0.0001, 0.0001, 1000)
-    # plt.subplot(2,2,3)
-    # plt.plot(x, fraunhofer(x, 0.01))
-    # plt.plot(x, fresnel(x, 0.01, N))
-    # x = n.linspace(-0.00001, 0.00001, 1000)
-    # plt.subplot(2,2,4)
-    # plt.plot(x, fraunhofer(x, 0.001))
-    # plt.plot(x, fresnel(x, 0.001, N))
-    # plt.show()
 
 
 def fraunhofer(x, L):
+    """
+    :param x: Point on screen
+    :param L: Distance between slit and screen
+    :return: Returns the intenstiy function computed with Fraunhofer approximation
+    """
     hypot = n.sqrt(x**2+L**2)
     beta = n.pi*100e-6/632e-9*x/hypot
     return (n.sin(beta)/beta)**2
 
 
 def fresnel(x, L, N):
+    """
+    :param x: Point on screen
+    :param L: Distance between slits and screen
+    :param N: Number of slits
+    :return: Returns the intensity function
+    For every point on the screen and for every slit the complex contribution is added.
+    """
     d = 100e-6/N
-    hypot = n.sqrt(x**2+L**2)
-    gamma = n.pi*d/632e-9*x/hypot
-    print((n.sin(N*gamma)/n.sin(gamma))**2)
-    return (n.sin(N*gamma)/n.sin(gamma))**2
+    wl = 632e-9
+    total = n.zeros_like(x, dtype=n.complex64)
+    for index, Point in enumerate(x):
+        for i in range(-N // 2, N//2):
+            hypot = n.sqrt(L**2+(abs(Point)+i*d)**2)
+            phase = 2*n.pi*(hypot % wl)/wl
+            total[index] += n.exp(phase * 1j)/N
+    return n.abs(total)
+
 
 
 main()
