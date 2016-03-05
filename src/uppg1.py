906f93dbb7adf82e8cc3221f55d2ac5c850a3976
jonasd90 <jonasd90@gmail.com>
2016-03-05 18:28:06 +0100

    added plots

--- src/uppg1.py
+++ src/uppg1.py
@@ -6,24 +6,37 @@ import matplotlib.pyplot as plt
 def main():
     plotReflection(1, 1.75)
     plotReflection(1.75, 1)
+
     plt.show()
 
 
 def plotReflection(n1, n2):
+
     a1 = n.array(n.linspace(0.1, n.pi / 2, 1000))
     a2 = n.arcsin(n1 * (n.sin(a1)) / n2)
+    print(a2)
     diffs = a1 - a2
     sums = a1 + a2
     Rs = n.abs(n.sin(diffs) / n.sin(sums)) ** 2
     Rp = n.abs(n.tan(diffs) / n.tan(sums)) ** 2
-    brewster = n.arctan(n2/n1)
-    totalRef = n.arcsin(n2/n1)
-
     plt.figure()
-    plt.plot((brewster, brewster), (-100, 100), "r")
-    plt.plot((totalRef, totalRef), (-100, 100), "y")
-    plt.plot(a1, Rs)
-    plt.plot(a1, Rp)
+    brewster = n.arctan(n2/n1)
+
+    # plt.subplot(2,1,1)
+    plt.plot((brewster, brewster), (-100, 100), "y", label='Brewster')
+    if n2 < n1:
+        totalRef = n.arcsin(n2/n1)
+        plt.plot((totalRef, totalRef), (-100, 100),"r" ,label='Total Reflection')
+
+    # plt.xlabel("?")
+    # plt.ylabel("Intensity")
+    plt.legend(loc=2)
+    # plt.subplot(2,1,2)
+    plt.plot(a1, Rs, "b" ,label='Rs')
+    plt.plot(a1, Rp, "g" ,label='Rp')
+    plt.xlabel("Radians")
+    plt.ylabel("Reflection")
+    plt.legend(loc=2)
     plt.ylim(0,1)
 
 
